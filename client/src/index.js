112954ec5460554ed1741c9be8437d68fd4f47ec
yamoo9 <yamoo9@naver.com>
2022-08-23 15:29:12 +0900

    learn. fetch API 활용

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,22 @@
 // ESM, Node.js (type="module")
-import { fetchBooks } from './utils/index.js';
+// import { fetchBooks } from './utils/index.js';
 
-fetchBooks(1200)
-  .then((response) => console.log(response))
-  .catch((error) => console.error(error.message));
+// fetchBooks(1200)
+//   .then((response) => console.log(response))
+//   .catch((error) => console.error(error.message));
+
+// http://localhost:4000/books
+
+async function fetckBooks() {
+  try {
+    const response = await fetch('http://localhost:4000/books');
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.name);
+    console.error(error.message);
+    console.error(error.stack);
+  }
+}
+
+fetckBooks();
