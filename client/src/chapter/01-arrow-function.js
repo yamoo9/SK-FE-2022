221833345b5ebc3ffc8da9b880545bf7da395012
yamoo9 <yamoo9@naver.com>
2022-08-23 10:43:16 +0900

    learn. expect 함수 활용 테스트 코드 작성

--- client/src/chapter/01-arrow-function.js
+++ client/src/chapter/01-arrow-function.js
@@ -1,3 +1,7 @@
+/* eslint-disable jest/no-standalone-expect */
+// CommonJS (Node.js) 모듈 불러오기
+const { expect } = require('expect');
+
 /* -------------------------------------------------------------------------- */
 /* ECMAScript NEXT를 사용해 함수를 작성합니다.                                       */
 /* -------------------------------------------------------------------------- */
@@ -10,11 +14,15 @@
 const fibonacci = n => (n <= 1) ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
 
 let result = fibonacci(8);
-console.log(result);
+
+expect(result).toBe(34);
+console.log('fibonacci(8) === 34 OK!');
 
 // 함수 (표현)식 → 화살표 함수식
 
 var numberWithComma = n => n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 
 result = numberWithComma(98000);
-console.log(result);
+
+expect(result).toBe('98,000');
+console.log('numberWithComma(98000) === "98,000" OK!');
