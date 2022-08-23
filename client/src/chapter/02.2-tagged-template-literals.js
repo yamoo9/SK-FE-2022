8082be42991113be0b465745ad42ef0446de0733
yamoo9 <yamoo9@naver.com>
2022-08-23 11:30:27 +0900

    learn. Tagged Template
    - REF. styled-components 문법
    - 커링 함수

--- client/src/chapter/02.2-tagged-template-literals.js
+++ client/src/chapter/02.2-tagged-template-literals.js
@@ -2,16 +2,62 @@
 /* ECMAScript NEXT를 사용해 함수를 작성합니다.                                       */
 /* -------------------------------------------------------------------------- */
 
+// Actual DOM
+// Virtual DOM
+
+// primitives [string, number, boolean, null, undefined, Symbol] vs. object [function, array, object]
+
 const virtualNode = {
   name: 'figure',
   className: 'tagged-template-literals',
   style: { cssText: '' },
 };
 
-const styled = () => {
-  // 코드를 작성합니다.
+// const styled = (cssRulesArray, node) => {
+//   node.style.cssText = cssRulesArray.reduce((result, cssRule) => result + cssRule, '');
+// };
+
+// tagged template
+// styled`
+//   ${virtualNode}
+//   font-weight: bold;
+//   font-size: 2rem;
+// `;
+
+// console.log(typeof virtualNode.style.cssText);
+
+// curring function
+// function expression
+// const $styled = function(node) {
+//   return function(cssRulesArray) {
+//     node.style.cssText = cssRulesArray.reduce(function(result, cssRule) {
+//       return result + cssRule
+//     }, '');
+//   }
+// };
+
+// arraw function expression
+const $styled = node => cssRulesArray => {
+  node.style.cssText = cssRulesArray.reduce((result, cssRule) => result + cssRule, '');
 };
 
-const $styled = () => {
-  // 코드를 작성합니다.
-};
+console.log('before:', virtualNode.style.cssText);
+
+// tagged template (excute function)
+$styled(virtualNode)`
+  font-weight: bold;
+  font-size: 2rem;
+`;
+
+console.log('after:', virtualNode.style.cssText);
+
+
+// DOM Script
+
+const abbrElement = document.querySelector('abbr');
+
+$styled(abbrElement)`
+  font-weight: 100;
+  font-size: 3rem;
+  letter-spacing: 0.4em;
+`;
