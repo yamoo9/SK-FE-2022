c5a165610c4737672192fd4e4787815024d48d9b
yamoo9 <yamoo9@naver.com>
2022-08-23 10:35:48 +0900

    learn. ES6 Arrow Function
    - 매개변수가 1개인 경우, 괄호 생략 가능
    - 한 줄로 값을 반환할 경우, return과 괄호 생략 가능

--- client/src/chapter/01-arrow-function.js
+++ client/src/chapter/01-arrow-function.js
@@ -2,11 +2,19 @@
 /* ECMAScript NEXT를 사용해 함수를 작성합니다.                                       */
 /* -------------------------------------------------------------------------- */
 
-function fibonacci(n) {
-  if (n <= 1) return 1;
-  return fibonacci(n - 1) + fibonacci(n - 2);
-}
+// 함수 선언문 → 화살표 함수식
 
-var numberWithComma = function (n) {
-  return n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-};
+// 문(값을 반환 X)
+// 식, 함수(값을 반환)
+
+const fibonacci = n => (n <= 1) ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
+
+let result = fibonacci(8);
+console.log(result);
+
+// 함수 (표현)식 → 화살표 함수식
+
+var numberWithComma = n => n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+
+result = numberWithComma(98000);
+console.log(result);
