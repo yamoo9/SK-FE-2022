61c7e5bf358d89f2ef7015669843d2820e1d2925
yamoo9 <yamoo9@naver.com>
2022-08-25 15:31:11 +0900

    learn. stateful component + life cycle methods
    - componentDidMount

--- src/components/RandomUser/RandomUser.jsx
+++ src/components/RandomUser/RandomUser.jsx
@@ -1,8 +1,28 @@
+import axios from 'axios';
 import { css } from '@emotion/css';
 import { Component } from 'react';
 import { Spinner } from './Spinner';
 
-// component
+// stateful component
+// container component
+// clas component
+
+// class component `life cycle`
+// ------------------------------------
+// constructor
+// static getDerivedStateFromProps
+// static getDerivedStateFromError
+// shouldComponentUpdate
+// render
+// ------------------------------------
+// getSnapshotBeforeUpdate
+// componentDidMount ← fetch request
+// componentDidUpdate
+// componentWillUnmount
+// componentDidCatch
+
+/* component ---------------------------------------------------------------- */
+
 class RandomUser extends Component {
   constructor(props) {
     super(props);
@@ -16,18 +36,70 @@ class RandomUser extends Component {
 
   render() {
     // 조건부 렌더링
+    const { user, error, loading } = this.state;
+
     // 상태 loading 값이 true인 경우
-    if (this.state.loading) {
-      return <Spinner />;
+    if (loading) {
+      return <Spinner height={60} />;
+    }
+
+    // 오류가 발생한 경우
+    if (error) {
+      return <div role="alert">{error.message}</div>;
     }
 
     // 그렇지 않은 경우
-    return <figure className={elementStyle}>{this.props.children}</figure>;
+    return (
+      <figure className={elementStyle}>
+        <img src={user.photo} alt="" />
+        <figcaption>{user.email}</figcaption>
+      </figure>
+    );
+  }
+
+  async fetchRandomUser() {
+    // console.log('call request');
+    try {
+      const {
+        data: { results },
+      } = await axios.get(
+        'https://randomuser.me/api/?inc=id,email,gender,name,nat,picture'
+      );
+      const randomUser = results[0];
+      this.setState({
+        user: {
+          name: `${randomUser.name.first} ${randomUser.name.last}`,
+          email: randomUser.email,
+          gender: randomUser.gender,
+          nat: randomUser.nat,
+          photo: randomUser.picture.large,
+        },
+      });
+    } catch (error) {
+      this.setState({
+        error /* { name, message, stack } */,
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+
+  componentDidMount() {
+    // 명령형 프로그래밍 가능
+    // 사이드 이펙트 (부수효과)
+    // React가 하는 일이 아닌 것들
+    // 1. 비동기 요청/처리
+    this.fetchRandomUser();
+    // 2. DOM 노드 접근/조작
+    // 3. 이벤트 구독/취소
+    // 4. 오류 경계 (오류 감지 및 처리)
   }
 }
 
 export default RandomUser;
 
-// component styles
+/* component styles --------------------------------------------------------- */
 
 const elementStyle = css``;
--- src/components/RandomUser/Spinner.jsx
+++ src/components/RandomUser/Spinner.jsx
@@ -1,3 +1,30 @@
-export const Spinner = () => {
-  return <figure>로딩 중...</figure>;
+import fidgetPath, {
+  ReactComponent as Fidget,
+} from 'assets/fidget.spinner.svg';
+
+import { css } from '@emotion/css';
+
+const spinnerStyle = css`
+  margin: 0;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+console.log(fidgetPath); // path string
+// SVGR: React Component
+// console.log(<Fidget title="로딩 중...." />);
+
+export const Spinner = ({ label, title, ...restProps }) => {
+  return (
+    <figure className={spinnerStyle}>
+      <Fidget title={title} {...restProps} />
+      <figcaption>{label}</figcaption>
+    </figure>
+  );
+};
+
+Spinner.defaultProps = {
+  label: '로딩 중...',
 };
