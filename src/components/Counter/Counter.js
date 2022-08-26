7965def740cb73837f67e8852610810f78bbde19
yamoo9 <yamoo9@naver.com>
2022-08-26 15:47:05 +0900

    learn. 성능 최적화
    - useCallback
    - useMemo

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -13,40 +13,51 @@ const StyledCounter = styled.div`
 export const Counter = ({ count: initialCount, step, min, max }) => {
   const [count, setCount] = React.useState(initialCount);
 
-  const incrementCount = () => {
+  const incrementCount = React.useCallback(() => {
     setCount((count) => {
       let updateCountValue = count + step;
       updateCountValue = updateCountValue > max ? max : updateCountValue;
       return updateCountValue;
     });
-  };
+  }, [max, step]);
 
-  const decrementCount = () => {
+  const decrementCount = React.useCallback(() => {
     setCount((count) => {
       let updateCountValue = count - step;
       updateCountValue = updateCountValue < min ? min : updateCountValue;
       return updateCountValue;
     });
-  };
+  }, [min, step]);
+
+  // 파생 상태
+  let isMin = count === min;
+  let isMax = count === max;
+
+  // + 버튼 렌더링 된 값을 메모
+  const memoizedIncButton = React.useMemo(
+    () => (
+      <Button label="카운트 증가" disabled={isMax} onClick={incrementCount}>
+        +
+      </Button>
+    ),
+    [isMax, incrementCount]
+  );
+
+  const memoizedDecButton = React.useMemo(
+    () => (
+      <Button label="카운트 감사" disabled={isMin} onClick={decrementCount}>
+        -
+      </Button>
+    ),
+    [isMin, decrementCount]
+  );
 
   return (
     <StyledCounter>
       <Output>{count}</Output>
       <Button.Group>
-        <Button
-          disabled={count === max}
-          label="카운트 증가"
-          onClick={incrementCount}
-        >
-          +
-        </Button>
-        <Button
-          disabled={count === min}
-          label="카운트 감사"
-          onClick={decrementCount}
-        >
-          -
-        </Button>
+        {memoizedIncButton}
+        {memoizedDecButton}
       </Button.Group>
     </StyledCounter>
   );
